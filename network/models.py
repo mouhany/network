8b4cd50d2e8dd0241949607de04a7ae6e77b43a8
Mouhany <mouhany@gmail.com>
2023-09-05 00:30:16 +0700

    Create models

--- network/models.py
+++ network/models.py
@@ -4,3 +4,17 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+    following = models.ManyToManyField("self", blank=True, related_name="followers", symmetrical=False)
+    
+
+class Post(models.Model):
+    content = models.CharField(max_length=255)
+    poster = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    likers = models.ManyToManyField(User, blank=True, related_name="likes")
+    date_posted = models.DateTimeField(auto_now_add=True)
+    
+    def num_of_likes(self):
+        return self.likers.all().count()
+    
+    class Meta:
+        ordering = ["-date_posted"]
