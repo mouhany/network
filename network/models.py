416e64e1bab6eb51786bd9df6c948eea1077a8c6
Mouhany <mouhany@gmail.com>
2023-09-06 05:59:07 +0700

    Edit model and start templating

--- network/models.py
+++ network/models.py
@@ -12,9 +12,10 @@ class Post(models.Model):
     poster = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
     likers = models.ManyToManyField(User, blank=True, related_name="likes")
     date_posted = models.DateTimeField(auto_now_add=True)
+    edited = models.BooleanField(default=False)
     
     def num_of_likes(self):
-        return self.likers.all().count()
+        return len(self.likers.all())
     
     class Meta:
         ordering = ["-date_posted"]
